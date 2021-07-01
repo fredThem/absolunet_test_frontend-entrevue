11ccf0d02972af5f28d70a5f017ed956e92c3dac
fredThem <fdesmarais.them@gmail.com>
2021-06-30 23:29:40 -0400

    to Scss, MUI theming and first components

---
+++ src/components/AppBar.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+// import { ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Fira Code'
+    ].join(','),
+  },
+});
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide
+      appear={false}
+      direction="down"
+      in={!trigger}
+    >
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function HideAppBar(props) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HideOnScroll {...props}>
+          <AppBar>
+            <Toolbar>
+              <Box
+                display="flex"
+                justifyContent="center"
+                flexGrow="1"
+                alignItems="center"
+                // spacing="4px"
+                // gap="3px"
+                sx={{ gap: '13px' }}
+              >
+
+                <Box flexGrow={1}>
+                  <Typography
+                    variant="h6"
+                    // mr="auto"
+                    // display="flex"
+                    // flexGrow="1"
+                  >
+                    Absolunet Frontend Test
+                  </Typography>
+                </Box>
+                <Typography
+                  variant="subtitle2"
+                  component="span"
+                  // fontFamily="Roboto"
+                  color="red"
+                >
+                  by Frédéric Desmarais
+                </Typography>
+              </Box>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
+        <Toolbar />
+        <Container>
+          <Box my={2}>
+            {[...new Array(12)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              )
+              .join('\n')}
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/dokdo';
+import '@fontsource/atomic-age';
+import '@fontsource/roboto';
 
 ReactDOM.render(
   <React.StrictMode>
